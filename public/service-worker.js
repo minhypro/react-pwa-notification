fa901e6d900892928f767c4303c44a2a360d4d18
minhypro <le.minh.y.95@gmail.com>
2024-08-27 15:36:44 +0700

    init

---
+++ public/service-worker.js
@@ -0,0 +1,33 @@
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+        body: data.message,
+        icon: 'icons/icon-72x72.png',
+        vibrate: [100, 50, 100],
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close(); // Close the notification
+
+    // This event waits for the browser to open a new tab/window with the specified URL
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+            for (let i = 0; i < clientList.length; i++) {
+                let client = clientList[i];
+                // Check if the window is already open with the desired URL
+                if (client.url === 'http://localhost:3001/' && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If the window isn't already open, open a new one
+            if (clients.openWindow) {
+                return clients.openWindow('http://localhost:3001/');
+            }
+        })
+    );
+});
---
+++ src/App.tsx
@@ -0,0 +1,146 @@
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import './App.css';
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
+import {useSubscribe} from "react-pwa-push-notifications";
+import toast, {Toaster} from 'react-hot-toast';
+import TextInput from "./components/Input";
+import axios from "axios";
+import Links from "./components/Links";
+import { QRCode, QRSvg } from 'sexy-qr';
+
+// in PROD use from .env
+const PUBLIC_KEY = 'BPLYWTHEplFjMQlUkIzXEzq1jIja6-Q0quAajJbhxPYchWhaOqP_2YVu0DhITXqSmoUKlp7i-W8pEv2RqmK4qxE';
+
+function App() {
+    const [loadingSubscribe, setLoadingSubscribe] = useState<boolean>(false)
+    const [loadingPush, setLoadingPush] = useState<boolean>(false)
+    const [pushId, setPushId] = useState<string>('');
+    const [message, setMessage] = useState<string>('World');
+    const [title, setTitle] = useState<string>('Hello');
+    const [subscribeId, setSubscribeId] = useState<string>('');
+    const [showSubscribe, setShowSubscribe] = useState<boolean>(true)
+
+    const onShowSubscribe = () => {
+        setShowSubscribe(true)
+    }
+    const onShowPush = () => {
+        setShowSubscribe(false)
+    }
+
+
+    const qrCode = useMemo(() => {
+        const qr =  new QRCode({
+            content: window.location.href,
+            ecl: 'M'
+        });
+        return new QRSvg(qr, {
+            fill: '#182026',
+            cornerBlocksAsCircles: true,
+            size: 200,
+            radiusFactor: 0.75,
+            cornerBlockRadiusFactor: 2,
+            roundOuterCorners: true,
+            roundInnerCorners: true,
+        }).svg
+    }, [])
+
+    const { getSubscription } = useSubscribe({publicKey: PUBLIC_KEY});
+
+    const onSubmitSubscribe = useCallback(async (e: React.FormEvent) => {
+        e.preventDefault();
+        setLoadingSubscribe(true)
+        try {
+            const subscription = await getSubscription();
+            await axios.post('http://192.168.1.36:3000/subscribe', {
+                subscription: subscription,
+                id: subscribeId
+            })
+            toast.success('Subscribe success');
+        } catch (e) {
+            console.warn(e);
+            toast.error(`Details console:: ${JSON.stringify(e)}`);
+        } finally {
+            setLoadingSubscribe(false)
+        }
+    }, [getSubscription, subscribeId]);
+
+    const onSubmitPush = useCallback(async (e: React.FormEvent) => {
+        e.preventDefault();
+        setLoadingPush(true)
+        try {
+            await axios.post('http://192.168.1.36:3000/send', {
+                message,
+                title,
+                id: pushId
+            })
+            toast.success('Push success');
+        } catch (e) {
+            toast.error('Details console');
+        } finally {
+            setLoadingPush(false)
+        }
+    }, [pushId, message, title]);
+
+    const onChange = useCallback((setState: React.Dispatch<React.SetStateAction<string>>) => (e: React.ChangeEvent<HTMLInputElement>) => {
+        setState(e.target.value);
+    }, []);
+
+    useEffect(() => {
+        FingerprintJS.load()
+            .then(fp => fp.get())
+            .then(result => {
+                setSubscribeId(result.visitorId)
+                setPushId(result.visitorId)
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            <main>
+                <div>
+                    <div className="message">
+                        <div className="title"> Use as PWA</div>
+                        <div>
+                            You need to install the site on your home screen.
+                            Subscribe to push notifications.
+                            Then you can test sending notifications.
+                        </div>
+                    </div>
+                    <div className={'qrCode'} dangerouslySetInnerHTML={{ __html: qrCode }} />
+                </div>
+                <div className="tabs">
+                    <div className={`tab-item`}>
+                        <button className={`tab ${showSubscribe ? 'active' : ''}`} onClick={onShowSubscribe}>Subscribe</button>
+                    </div>
+                    <div className={`tab-item`}>
+                        <button className={`tab ${!showSubscribe ? 'active' : ''}`} onClick={onShowPush}>Push</button>
+                    </div>
+                </div>
+                {!showSubscribe && <div className="send">
+                    <form onSubmit={onSubmitPush}>
+                        <div className="title">Notification</div>
+                        <TextInput id="idSubscribe" placeholder="id" value={pushId}
+                                   onChange={onChange(setPushId)}/>
+                        <TextInput id="title" placeholder="title" value={title} onChange={onChange(setTitle)}/>
+                        <TextInput id="message" placeholder="message" value={message} onChange={onChange(setMessage)}/>
+                        <button className={loadingPush ? 'loading' : ''}  type="submit">Send</button>
+                    </form>
+                </div>}
+                {showSubscribe &&  <div className="send">
+                    <form onSubmit={onSubmitSubscribe}>
+                        <div className="title">Your Id</div>
+                        <TextInput id="fingerprint" placeholder="Your id" value={subscribeId}
+                                   onChange={onChange(setSubscribeId)}/>
+                        <button className={loadingSubscribe ? 'loading' : ''} type="submit">Send</button>
+                    </form>
+                </div> }
+                <div>
+                    <Links />
+                </div>
+            </main>
+            <Toaster/>
+        </div>
+    );
+}
+
+export default App;
